4c9fe023a3e045a43ed1dcb260e6c1c282645813
Abdulmalik Aremu <aremzy2018@gmail.com>
2025-02-21 20:34:15 +0100

    fixing task one

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,14 +1,9 @@
 from .models import *
+library_name = ''
+library = Library.objects.get(name= library_name)
+library.books.all()
 
+author_name = ''
+Book.objects.get(author = author_name)
 
-
-def retrieve_book():
-    author = input("Who's Books do you want to read?")
-    Book.objects.get(author = author)
-
-def retrieve_Library():
-    Library.objects.all()
-
-def get_librarian():
-    library_name = input("What Librarian are you looking for?")
-    Librarian.objects.get(library =library_name)
+librarian = Librarian.objects.get(library =library_name)
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render #, redirect
 from django.views.generic import DetailView
 from django.contrib.auth.forms import UserCreationForm
-#from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
