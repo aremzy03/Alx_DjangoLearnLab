1d7975502f856765248371e1e8b1525fc64ac0c3
Abdulmalik Aremu <aremzy2018@gmail.com>
2025-02-21 17:28:54 +0100

    made code better

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
 from .views import *
 urlpatterns = [
     path("", index, name="index"),
 	path("books/", display_books, name="books"),
     path("library/<int:pk>/", Librarybook.as_view(), name="library"),
     path("register/", RegisterUserView.as_view(), name="register"),
-    path("login/", login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
+    #path("login/", login_view, name="login"),
+    #path("login/", LoginView.as_view(), name="login"),
+    path("login/", Login_view.as_view(), name="login"),
+    #path("logout/", logout_view, name="logout"),
+    path("logout/", Logout_view.as_view(), name="logout"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render #, redirect
 from django.views.generic import DetailView
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate, login, logout
+#from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
 from .models import *
 
 # Create your views here.
@@ -19,22 +20,22 @@ def display_books(request):
     context = {'books': books}
     return render(request, 'book_list.html', context)
 
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('index')
-        else:
-            messages.error(request, 'Username or Password is incorrect')
-    return render(request, 'login.html')
+# def login_view(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return redirect('index')
+#         else:
+#             messages.error(request, 'Username or Password is incorrect')
+#     return render(request, 'login.html')
 
-def logout_view(request):
-    logout(request)
-    messages.success(request, 'You have been logged out')
-    return redirect(login_view)
+# def logout_view(request):
+#     logout(request)
+#     messages.success(request, 'You have been logged out')
+#     return redirect(login_view)
         
 class Librarybook(DetailView):
     model = Library
@@ -51,3 +52,12 @@ class RegisterUserView(CreateView):
     form_class = UserCreationForm
     template_name = 'register.html'
     success_url = reverse_lazy('login')
+
+class Login_view(LoginView):
+    template_name = 'login.html'
+    
+    def get_success_url(self):
+        return reverse_lazy('index')
+
+class Logout_view(LogoutView):
+    template_name = 'logout.html'
