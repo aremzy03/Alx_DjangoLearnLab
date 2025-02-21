590ae027b0aaa8229fe48e63d19412a3aa536dfc
Abdulmalik Aremu <aremzy2018@gmail.com>
2025-02-21 20:37:37 +0100

    fixing task one

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,6 +4,7 @@ library = Library.objects.get(name=library_name)
 library.books.all()
 
 author_name = ''
-Book.objects.get(author = author_name)
+author = Author.objects.get(author = author_name)
+Book.objects.filter(author=author)
 
 librarian = Librarian.objects.get(library =library_name)
