478d53eb013f46551d38ed0a059b73478611ce74
Abdulmalik Aremu <aremzy2018@gmail.com>
2025-03-05 13:43:56 +0100

    django rest framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import generics, viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.authentication import TokenAuthentication
 from .serializers import BookSerializer
 from .models import Book
@@ -9,6 +9,8 @@ from .models import Book
 
 
 class BookList(generics.ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
