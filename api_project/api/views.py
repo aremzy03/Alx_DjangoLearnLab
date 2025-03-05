0f28ee3f46ff254fea8d3300cf5d140f16f5f314
Abdulmalik Aremu <aremzy2018@gmail.com>
2025-03-05 13:38:58 +0100

    django rest framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,13 +2,12 @@ from rest_framework.response import Response
 from rest_framework import generics, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
-from .serilizers import Book, BookSerializer
+from .serializers import Book, BookSerializer
 
 # Create your views here.
 
 
 class BookList(generics.ListAPIView):
-    
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
