dc17d42487067b8ca0bdf67e922531a2b97c183d
Abdulmalik Aremu <aremzy2018@gmail.com>
2025-03-05 13:40:14 +0100

    django rest framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,8 @@ from rest_framework.response import Response
 from rest_framework import generics, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
-from .serializers import Book, BookSerializer
+from .serializers import BookSerializer
+from .models import Book
 
 # Create your views here.
 
