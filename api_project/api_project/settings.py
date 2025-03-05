df1e143702cf3f09f0a49f0245b26388b5732917
Abdulmalik Aremu <aremzy2018@gmail.com>
2025-03-05 13:44:36 +0100

    django rest framework (missing s)

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.permission.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticated',
     ),
 }
 
