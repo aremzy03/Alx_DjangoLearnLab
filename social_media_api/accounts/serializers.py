e942bae871edd49e66e015946ea024ee1d6bbd7f
Abdulmalik Aremu <aremzy2018@gmail.com>
2025-03-24 11:57:40 +0100

    added token creation view

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,29 +1,25 @@
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate
+
+from django.contrib.auth import get_user_model
 from .models import CustomUser
 
 #serilizer classes here
 class CustomUserSerilizer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['name', 'bio', 'profile_picture', 'followers']
+        fields = ['bio', 'profile_picture', 'followers']
 
-class UserLoginSerializer(serializers.ModelSerializer):
-    username = serializers.CharField()
+class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=150, write_only=True)
-    token = serializers.CharField(read_only=True)
     
-    def validate(self, attrs):
-        
-        user = authenticate(username=attrs['username'], password=attrs['password'])
-        if not user:
-            raise serializers.ValidationError('Invalid Credentials')
-        attrs['user'] = user
-        return attrs
-    
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'password']
+
     def create(self, validated_data):
-        user = validated_data['user']
-        token,_ = Token.objects.get_or_create(user=user)
-        return {'token': token.key}
+        #user = validated_data['user']
+        user = get_user_model().objects.create_user(**validated_data)
+        token = Token.objects.create(user=user)
+        return user
     #get_user_model().objects.create_user Token.objects.create
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,25 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import CreateView
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from rest_framework.mixins import UpdateModelMixin
-from rest_framework.generics import UpdateAPIView, RetrieveAPIView
+from rest_framework import generics
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
 from .models import CustomUser
-from .serializers import CustomUserSerilizer, UserLoginSerializer
+from .serializers import CustomUserSerilizer, UserRegisterSerializer
 
 # Create your views here.
 
 
-class RegisterUser(CreateView):
-    form_class = UserCreationForm
-    template_name = ''
-    success_url = reverse_lazy('create-profile')
+class RegisterUser(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserRegisterSerializer
+    permission_classes = [AllowAny]
 
 
 def create_token(request):
@@ -27,16 +29,19 @@ def create_token(request):
     return HttpResponse(request, f'this is you token:{token.key}')
 
 
-class UserLogin(LoginView):
-    template_name = ''
-    success_url = reverse_lazy('profile')
-
-@api_view(['POST'])
-def login(request):
-    serializer = UserLoginSerializer(data=request.data)
-    if serializer.is_valid():
-        return Response(serializer.validated_data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UserLogin(APIView):
+    permission_classes = [AllowAny]
+    
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+	
 
 
 class UserLogout(LogoutView):
