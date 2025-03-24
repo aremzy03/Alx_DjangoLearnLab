dcf7e3a534aa99cb7a384fa5b0964e097c322c32
Abdulmalik Aremu <aremzy2018@gmail.com>
2025-03-24 09:56:39 +0100

    added token creation view

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('logout/', UserLogout.as_view(), name='user-logout'),
     path('profile/',ViewProfile.as_view(), name='profile'),
     path('profile/update/', UpdateProfile.as_view(), name='update-profile'),
+    path('create-token/', create_token, name='create-token'),
 ]
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from rest_framework.mixins import UpdateModelMixin
 from rest_framework.generics import UpdateAPIView, RetrieveAPIView
+from rest_framework.authtoken.models import Token
 from .models import CustomUser
 from .serilizers import CustomUserSerilizer
 
@@ -15,6 +17,11 @@ class RegisterUser(CreateView):
     form_class = UserCreationForm
     template_name = ''
     success_url = reverse_lazy('create-profile')
+
+
+def create_token(request):
+    token = Token.objects.create(user=request.user)
+    return HttpResponse(request, f'this is you token:{token.key}')
 
 
 class UserLogin(LoginView):
