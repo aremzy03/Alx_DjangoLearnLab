36a0254815e6b7bae243faa0e5b50a914a4e103b
Abdulmalik Aremu <aremzy2018@gmail.com>
2025-03-27 12:11:06 +0100

    deployment config complete

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -103,8 +103,11 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'social_media',
+        'PORT': '8080',
+        'USER': 'ROOT',
+        'PASSWORD': 'aremzyboss',
     }
 }
 
